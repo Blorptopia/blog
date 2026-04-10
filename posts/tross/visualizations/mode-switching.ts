c07c0482cdc38b30ac2f5572c813380112c565e8
Blorptopia <make-an-issue-on-my-github-README-repo-if-you-wish-to-contact-me@example.com>
2026-04-10 11:00:00 +0200

    add mode switching visualization to tross post

---
+++ posts/tross/visualizations/mode-switching.ts
@@ -0,0 +1,139 @@
+import { Task } from "@lit/task";
+import { css, html, LitElement, type CSSResultGroup, type HTMLTemplateResult } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+import { createRef, ref, type Ref } from "lit/directives/ref.js";
+
+
+@customElement("bb-tross-mode-switching-visualization")
+export class TrossModeSwitchingVisualization extends LitElement {
+	// Props
+	@property()
+	public text: string;
+	@property()
+	public updateInterval: number;
+
+	// State
+	@state()
+	private parserMode: ParserMode = "normal";
+
+	// Elements
+	private textRef: Ref<HTMLElement>;
+
+	public constructor() {
+		super();
+		this.text = "";
+		this.updateInterval = 100;
+		
+		this.textRef = createRef();
+
+		new Task(this, {
+			args: () => [this.text] as const,
+			task: async ([text], {signal}) => {
+				await this.loopAnimation(text, signal);	
+			},
+			onError: (error) => {
+				console.error("failed to animate", error);
+			}
+		});
+	}
+	protected render(): HTMLTemplateResult {
+		return html`
+			<div id="mode-pill">
+				<span id="active-mode" data-mode=${this.parserMode}>
+					${this.parserMode}
+				</span>
+				<span>mode</span>
+			</div>
+			<code ${ref(this.textRef)}>${this.text}</code>
+		`;
+	}
+	private async loopAnimation(text: string, signal: AbortSignal): Promise<void> {
+		const highlightId = "parser-cursor";
+		let highlight = CSS.highlights.get(highlightId);
+		if (highlight === undefined) {
+			highlight = new Highlight();
+			CSS.highlights.set(highlightId, highlight);
+		}
+
+		let characterIndex: number = 0;
+		let cursorRange: AbstractRange | undefined = undefined;
+		this.parserMode = "normal";
+		while (true) {
+			if (cursorRange !== undefined) {
+				highlight.delete(cursorRange);
+				cursorRange = undefined;
+			}
+			if (signal.aborted) {
+				return;
+			}
+			const characterAtIndex = text[characterIndex];
+			if (characterAtIndex === undefined) {
+				characterIndex = 0;
+				this.parserMode = "normal";
+				await new Promise(resolve => setTimeout(resolve, this.updateInterval));
+				continue;
+			}
+			if (characterAtIndex === "\"") {
+				if (this.parserMode === "normal") {
+					this.parserMode = "escaped";
+				} else {
+					this.parserMode = "normal";
+				}
+			}
+			const textElement = this.textRef.value;
+			if (textElement !== undefined) {
+				const textElementTextNode = textElement.childNodes[1]!;
+				console.log(textElementTextNode);
+				const characterRange = new Range();
+				characterRange.setStart(textElementTextNode, characterIndex);
+				characterRange.setEnd(textElementTextNode, characterIndex + 1);
+				cursorRange = characterRange;
+				highlight.add(cursorRange);
+				console.log(`highlighted ${characterIndex}`);
+			}
+			
+			characterIndex++;
+			await new Promise(resolve => setTimeout(resolve, this.updateInterval));
+		}
+	}
+
+	static styles?: CSSResultGroup = css`
+		:host {
+			display: flex;
+			flex-direction: column;
+			gap: .5rem;
+
+			border: 0.1rem solid light-dark(black, grey);
+			padding: 1rem;
+		}
+		#mode-pill {
+			display: flex;
+			background: #121212;
+			color: white;
+		}
+		#mode-pill > * {
+			display: flex;
+			justify-content: center;
+			padding: .5rem;
+		}
+		#mode-pill,#active-mode {
+			width: max-content;
+		}
+		#active-mode[data-mode="normal"] {
+			background: green;
+		}
+		#active-mode[data-mode="escaped"] {
+			background: red;
+		}
+		code {
+			background: #262626;
+			padding: 1rem;
+			color: white;
+		}
+		::highlight(parser-cursor) {
+			background: red;
+		}
+	`;
+}
+
+type ParserMode = "normal" | "escaped";
